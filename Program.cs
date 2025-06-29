3b27562fdcdd510d7c16300010bc93877b62bef0
reignetteV <irishreignette@gmail.com>
2025-06-29 20:28:39 +0800

    Dice and Guess + Added options

--- Program.cs
+++ Program.cs
@@ -4,16 +4,40 @@ class Act1
     static void Main(string[] args)
     {
         Console.WriteLine("-----------------------------------------");
-        Console.WriteLine("Hi! welcome to the C# Calculator program!");
+        Console.WriteLine("Hi! welcome to the C# Calculator or Dice or Guess the Number program!");
         Console.WriteLine("-----------------------------------------");
         Console.WriteLine("\nPlease enter your name:");
         string studentName = Console.ReadLine();
 
         sayHi(studentName);
 
-        Console.WriteLine("-----------------------------------------"); }
+        Console.WriteLine("-----------------------------------------");
 
-       public static void Calculator()
+        Console.WriteLine("Please choose an option:");
+        Console.WriteLine("1. Calculator");
+        Console.WriteLine("2. Dice");
+        Console.WriteLine("3. Guess the Number");
+
+        Console.WriteLine("-----------------------------------------");
+
+        var option = Console.ReadLine();
+        switch (option)
+        {
+            case "1":
+                Calculator();
+                break;
+            case "2":
+                Dice();
+                break;
+            case "3":
+                Guess();
+                break;
+            default:
+                Console.WriteLine("Invalid option. Please enter 1, 2, or 3.");
+                break;
+        }
+
+        static void Calculator()
         {
             double num1 = 0;
             double num2 = 0;
@@ -74,8 +98,57 @@ class Act1
             }
             Console.ReadKey();
         }
+
+        static void Dice()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("C# Dice program!");
+            Console.WriteLine("-----------------------------------------");
+            Random random = new Random();
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("You rolled a " + diceRoll + "!");
+            Console.ReadKey();
+        }
+
+        static void Guess()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("C# Guess the Number!");
+            Console.WriteLine("-----------------------------------------");
+
+            Random rand = new Random();
+            int ranNum = rand.Next(1, 101);
+            int guess = 0;
+            int attempts = 0;
+            Console.WriteLine("Guess a number between 1 and 100:");
+
+            while (guess != ranNum)
+            {
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    if (guess < ranNum)
+                    {
+                        Console.WriteLine("Your guess is too low! Try again");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guess is too high! Try again");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Your guess must be a number");
+                    attempts--;
+                }
+                attempts++;
+            }
+            Console.WriteLine("Congratulations! You guessed the number " + ranNum + " in " + attempts + " attempts.");
+            Console.ReadLine();
+        }
         static void sayHi(string name)
         {
             Console.WriteLine("\nHi " + name + "!");
         }
     }
+}
